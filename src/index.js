5aba3c8f3aeaa9182929957d3cff5ce0f0108e49
Minhazur Rahman <minhazur.rony@gmail.com>
2017-12-11 23:03:49 +0600

    Add Modification on Components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./Components/SearchBar";
 import WeatherDetails from "./Components/WeatherDetails";
 
-const App = () => (
-  <div>
-    <div>
-      <h1> React Weather App</h1>
-      <SearchBar />
-      <WeatherDetails />
-    </div>
-  </div>
-);
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> React Weather App</h1>
+        <SearchBar />
+        <WeatherDetails />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
