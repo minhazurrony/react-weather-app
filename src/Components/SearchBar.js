915351f7e3ceb84239721641168dafa0dccaecbc
Minhazur Rahman <minhazur.rony@gmail.com>
2017-12-11 19:23:12 +0600

    Add Necessary Components

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = { term: "" };
+  }
   render() {
     return (
       <div>
         <form>
-          <input type="text" placeholder="City Name" />
+          <input
+            onChange={this.onInputChange}
+            type="text"
+            placeholder="City Name"
+          />
         </form>
       </div>
     );
+  }
+  onInputChange(event) {
+    console.log(event.target.value);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./Components/App";
+import SearchBar from "./Components/SearchBar";
+import WeatherDetails from "./Components/WeatherDetails";
+
+const App = () => (
+  <div>
+    <div>
+      <h1> React Weather App</h1>
+      <SearchBar />
+      <WeatherDetails />
+    </div>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
